17bc6e535907c44339a8cb3c72e2cb9db4b94eb0
Leo Muona <leo.muona@fraktio.fi>
2024-03-27 12:06:49 +0200

    add iphone safe area

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  type EdgeInsets,
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
-export default function App() {
+function AppWithContext() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles({ insets }).container}>
       <StatusBar style="auto" />
+      <ScrollView>
+        {Array(200)
+          .fill(1)
+          .map((_v, index) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            <Text key={index}>{`SUDO WGET LOCALHOST ${index}`}</Text>
+          ))}
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <AppWithContext />
+    </SafeAreaProvider>
+  );
+}
+
+type StylesProps = {
+  insets: EdgeInsets;
+};
+
+const styles = ({ insets }: StylesProps) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      // safe area insets
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+    },
+  });
